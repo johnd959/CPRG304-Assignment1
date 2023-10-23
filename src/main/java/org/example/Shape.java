a7ae99993fa4c473d95ab660202b9fff0a963b4c
John Dao <104119657+johnd959@users.noreply.github.com>
2023-10-23 15:38:19 -0600

    Fixed

--- src/main/java/org/example/Shape.java
+++ src/main/java/org/example/Shape.java
@@ -1,17 +1,12 @@
 package org.example;
 
-<<<<<<< HEAD:src/main/java/org/example/shape.java
 import java.util.Comparator;
-
-public abstract class shape implements Comparable {
-=======
 public abstract class Shape implements Comparable {
->>>>>>> Ahmad:src/main/java/org/example/Shape.java
 
     public int height;
 
 
-    public shape(int height)
+    public Shape(int height)
     {
         this.height = height;
     }
