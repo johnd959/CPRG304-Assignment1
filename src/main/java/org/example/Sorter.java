fdf285d9449904c96e8d09c28d7a8bcc69c51bd3
John Dao <104119657+johnd959@users.noreply.github.com>
2023-10-25 15:52:35 -0600

    Fial touches

--- src/main/java/org/example/Sorter.java
+++ src/main/java/org/example/Sorter.java
@@ -107,60 +107,38 @@ public class Sorter<T> {
         return boundary;
     }
 
-    public void BucketSort(T [] array, int numberOfBuket, char sortQuality) {
-        if (numberOfBuket <= 0) throw new IllegalArgumentException("Invalid bucket count");
+    public void HeapSort(T[] arr) {
+        int n = arr.length;
 
+        for (int i = n / 2 - 1; i >= 0; i--)
+            heapify(arr, n, i);
 
-        // Determine minimum and maximum values
-        T minValue = array[0];
-        T maxValue = array[0];
-        for (int i = 1; i < array.length; i++) {
-            if (comparator.compare(array[i], minValue) < 0) minValue = array[i];
-            else if (comparator.compare(array[i], maxValue) < 0) maxValue = array[i];
-        }
+        for (int i = n - 1; i > 0; i--) {
+            T temp = arr[0];
+            arr[0] = arr[i];
+            arr[i] = temp;
 
-        // Initialize buckets
-        double interval = ((double) (retrieveValue((Shape)maxValue, sortQuality) - retrieveValue((Shape) minValue, sortQuality) + 1)) / numberOfBuket;
-        ArrayList<ArrayList<Shape>> buckets = new ArrayList<ArrayList<Shape>>(numberOfBuket);
-        for (int i = 0; i < numberOfBuket; i++) {
-            buckets.add(new ArrayList<Shape>());
-        }
-
-        // Distribute input data into buckets
-        for (int i = 0; i < array.length; i++) {
-            buckets.get((int) ((retrieveValue((Shape) array[i], sortQuality) - retrieveValue((Shape) minValue, sortQuality)) / interval)).add((Shape) array[i]);
-        }
-
-        // Sort buckets and merge them to get sorted array
-        int pointer = 0;
-        for (int i = 0; i < buckets.size(); i++) {
-            Collections.sort(buckets.get(i));
-            for (int j = 0; j < buckets.get(i).size(); j++) {
-                array[pointer] = (T) buckets.get(i).get(j);
-                pointer++;
-            }
+            heapify(arr, i, 0);
         }
     }
 
+    private void heapify(T[] arr, int n, int i) {
+        int largest = i;
+        int left = 2 * i + 1;
+        int right = 2 * i + 2;
 
-    private double retrieveValue(Shape shape, char sortQuality)
-    {
-        switch(sortQuality)
-        {
-            case 'h':
-            {
-                return shape.getHeight();
-            }
-            case 'v':
-            {
-                return shape.calculateVolume();
-            }
-            case 'a':
-            {
-                return shape.calculateBaseArea();
-            }
+        if (left < n && comparator.compare(arr[left], arr[largest]) > 0)
+            largest = left;
+
+        if (right < n && comparator.compare(arr[right], arr[largest]) > 0)
+            largest = right;
+
+        if (largest != i) {
+            T swap = arr[i];
+            arr[i] = arr[largest];
+            arr[largest] = swap;
+
+            heapify(arr, n, largest);
         }
-
-        throw new IllegalArgumentException("Object passed into retrieveValue is not a shape");
     }
 }
