a79949f6719fe258ea0a5eb06da2e92499d7dcc6
SyedJawadRaza-Baquar <jawad.raza72021@gmail.com>
2023-10-26 01:07:11 -0600

    added comments

--- src/main/java/org/example/shapes/Shape.java
+++ src/main/java/org/example/shapes/Shape.java
@@ -1,20 +1,28 @@
 package org.example.shapes;
 
 import java.util.Comparator;
+
+//Abstract class for the three-dimensional shapes
 public abstract class Shape implements Comparable<Shape> {
 
+    //Abstract method to calculate the volume of the shape
     public abstract double calculateVolume();
 
+    //abstract method to calculate the base area of the shape
     public abstract double calculateBaseArea();
 
+    //Abstract method to get the height of the shape
     public abstract double getHeight();
 
+    //Implementation of the compareTo method to compare the shapes by their height
     @Override
     public int compareTo(Shape other) {
         return Double.compare(this.getHeight(), other.getHeight());
     }
 
+    //Implementing comparator to compare the shapes by their base area
     public static Comparator<Shape> baseAreaComparator = Comparator.comparingDouble(Shape::calculateBaseArea);
 
+    //Implementing comparator to compare the shapes by their volume
     public static Comparator<Shape> volumeComparator = Comparator.comparingDouble(Shape::calculateVolume);
 }
--- src/main/java/org/example/shapes/shapeComparator.java
+++ src/main/java/org/example/shapes/shapeComparator.java
@@ -2,6 +2,7 @@ package org.example.shapes;
 
 import java.util.Comparator;
 
+//shapeComparator class to compare the shapes based on the user input i.e. height, volume or base area
 public class shapeComparator implements Comparator<Shape>{
     private char compareType;
 
@@ -16,12 +17,16 @@ public class shapeComparator implements Comparator<Shape>{
     @Override
     public int compare(Shape shape1, Shape shape2) {
         switch (compareType) {
+            //compare by height
             case 'h':
                 return shape1.compareTo(shape2);
+            //compare by volume
             case 'v':
                 return Shape.volumeComparator.compare(shape1, shape2);
+            //compare by base area
             case 'a':
                 return Shape.baseAreaComparator.compare(shape1, shape2);
+            //throwing exception in case of wrong user input
             default:
                 throw new IllegalArgumentException(compareType + " is not a valid input. Please try again.");
         }
